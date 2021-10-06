ddcb922c17d542ef5d866a11c463c15bcd8d56d2
leomani3 <xboxlive84320@hotmail.fr>
2021-10-06 08:55:39 +0200

    Grapple mechanic : ray projection

--- Assets/_Project/Scripts/GrappleLevelMechanic.cs
+++ Assets/_Project/Scripts/GrappleLevelMechanic.cs
@@ -1,3 +1,5 @@
+using System;
+using MyBox;
 using UnityEngine;
 
 public class GrappleLevelMechanic : MonoBehaviour, ILevelMechanic
@@ -7,13 +9,21 @@ public class GrappleLevelMechanic : MonoBehaviour, ILevelMechanic
     public float grapplePullingSpeed;
 
     private bool _connected;
-    
+    private Vector3 _worldMousePos;
+    private Camera _mainCam;
+
+    private void Awake()
+    {
+        _mainCam = Camera.main;
+    }
+
     public void Interact()
     {
         if (!_connected)
         {
-            Ray ray = new Ray(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            Debug.DrawRay(ray.origin, ray.direction);
+            _worldMousePos = _mainCam.ScreenToWorldPoint(Input.mousePosition.SetZ(Mathf.Abs(_mainCam.transform.position.z)));
+            Ray ray = new Ray(transform.position, _worldMousePos - transform.position);
+            Debug.DrawRay(ray.origin, ray.direction * 100);
         }
         else
         {
